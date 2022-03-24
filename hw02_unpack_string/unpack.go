f193517f27e3719ecf917bc9a2f96d60ddd3ba2e
popovis <popovis@mos-team.ru>
2022-03-24 23:59:55 +0300

    changed after linter

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,12 +11,11 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(changedString string) (string, error) {
-
 	if len(changedString) == 0 {
 		return "", nil
 	}
 
-	if isStringCorrect(changedString) == false {
+	if !isStringCorrect(changedString) {
 		return "", ErrInvalidString
 	}
 
@@ -25,51 +24,38 @@ func Unpack(changedString string) (string, error) {
 	runes := []rune(changedString)
 
 	for i, str := range runes {
-
 		if unicode.IsDigit(str) {
-
 			repeatCount, _ := strconv.Atoi(string(str))
 
 			if repeatCount == 0 {
-
-				newString, _ := removeLastElement(result)
+				newString := removeLastElement(result)
 				result.Reset()
 				result.WriteString(newString)
-
 			} else {
 				result.WriteString(strings.Repeat(string(runes[i-1]), repeatCount-1))
 			}
-
 		} else {
 			result.WriteString(string(str))
 		}
-
 	}
 
 	return result.String(), nil
 }
 
 func isStringCorrect(checkString string) bool {
-
 	if unicode.IsDigit(rune(checkString[0])) {
 		return false
 	}
 
 	matched, _ := regexp.MatchString(`\d{2,}`, checkString)
 
-	if matched == true {
-		return false
-	}
-
-	return true
+	return !matched
 }
 
-func removeLastElement(buildString strings.Builder) (string, error) {
-
-	str := buildString.String()
-	if len(str) > 0 {
-		return str[:len(str)-1], nil
+func removeLastElement(buildString strings.Builder) string {
+	if str := buildString.String(); len(str) > 0 {
+		return str[:len(str)-1]
 	}
 
-	return "", nil
+	return ""
 }
